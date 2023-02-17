3c626cb24128b9e27add934e5080f66add62edc5
steadfasterX <steadfasterX@gmail.com>
2023-02-17 12:08:28 +0100

    proc: cmdline: add / replace safetynet flags
    this will set / replace all known safetynet flags
    by those known to show a locked device state.
    note: this alone will not allow bypassing safetynet anymore,
    read all details and further steps about attestation here:
    https://github.com/kdrag0n/safetynet-fix
    
    This will not set "ro.boot.flash.locked = 1" though to avoid issues
    if someone do not want to lock his bootloader.
    on the other hand this will require to lock the bootloader to fully
    integrate the safetynet flags and.. I like it that way.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -22,9 +22,48 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+/**
+    Set all known SafetyNet flags to fake a full locked and
+    verified device. SafetyNet checks more then just that,
+    especially work-around key attestation:
+    https://github.com/kdrag0n/safetynet-fix
+**/
+static void remove_flag(char *cmd, const char *flag)
+{
+        char *start_addr, *end_addr;
+
+        /* Ensure all instances of a flag are removed */
+        while ((start_addr = strstr(cmd, flag))) {
+                end_addr = strchr(start_addr, ' ');
+                if (end_addr)
+                        memmove(start_addr, end_addr + 1, strlen(end_addr));
+                else
+                        *(start_addr - 1) = '\0';
+        }
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+        remove_flag(cmd, "androidboot.enable_dm_verity=");
+        remove_flag(cmd, "androidboot.secboot=");
+        remove_flag(cmd, "androidboot.veritymode=");
+        remove_flag(cmd, "bootlock=");
+        remove_flag(cmd, "androidboot.vbmeta.device_state");
+        remove_flag(cmd, "androidboot.verifiedbootstate=");
+}
+
+static void set_safetynet_flags(char *cmd)
+{
+	char sflags[400] = " androidboot.enable_dm_verity=1 androidboot.secboot=enabled androidboot.veritymode=enforcing androidboot.vbmeta.device_state=locked androidboot.verifiedbootstate=green";
+	memcpy(cmd, strcat(cmd,sflags), sizeof(sflags) + sizeof(cmd));
+}
+
 static int __init proc_cmdline_init(void)
 {
+	remove_safetynet_flags(saved_command_line);
+	set_safetynet_flags(saved_command_line);
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+
 	return 0;
 }
 fs_initcall(proc_cmdline_init);
